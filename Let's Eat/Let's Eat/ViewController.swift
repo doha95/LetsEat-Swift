a2898cf2ee096a5a8b2908bf38a62ea96df81edb
Doha Tubaileh <doha.tubaileh@gmail.com>
2020-12-19 00:11:47 +0200

    setup project

---
+++ Let's Eat/Let's Eat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Let's Eat
+//
+//  Created by Doha Tubaileh on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
