1076ab657316874423934996f6df2799760d0b12
Doha Tubaileh <doha.tubaileh@gmail.com>
2021-01-30 11:53:11 +0200

    starting with the Explore Model

---
+++ Let's Eat/Let's Eat/Resources/Model/ExploreItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreItem.swift
+//  Let's Eat
+//
+//  Created by Doha Tubaileh on 30/1/2021.
+//
+
+import Foundation
+
+struct ExploreItem {
+    var name : String;
+    var image : String;
+}
