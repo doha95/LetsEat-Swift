016942d889bfb1e024e6756d9f98061127bed32f
Doha Tubaileh <doha.tubaileh@gmail.com>
2021-02-03 09:23:38 +0200

    adding explore custom cell

---
+++ Let's Eat/Let's Eat/Explore/View/ExploreCell.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreCellCollectionViewCell.swift
+//  Let's Eat
+//
+//  Created by Doha Tubaileh on 2/2/2021.
+//
+
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+   
+    @IBOutlet weak var imgExplore: UIImageView!
+    @IBOutlet weak var lblname: UILabel!
+    
+}
