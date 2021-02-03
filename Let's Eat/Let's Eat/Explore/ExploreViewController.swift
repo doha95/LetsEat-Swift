d86d980dd9b74889c4fec1fedcf3510f9986c030
Doha Tubaileh <doha.tubaileh@gmail.com>
2021-02-03 09:24:19 +0200

    connect between the explore model and the controller

--- Let's Eat/Let's Eat/Explore/ExploreViewController.swift
+++ Let's Eat/Let's Eat/Explore/ExploreViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 class ExploreViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
     
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView!;
+    let manager = ExploreDataManager();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        //Fetch all the resturans
+        manager.fetch();
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20;
+        return manager.numberOfItems();
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath);
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath) as! ExploreCell;
+        let item = manager.explore(at: indexPath);
+        cell.lblname.text = item.name;
+        cell.imgExplore.image = UIImage(named: item.image);
         return cell;
     }
     
