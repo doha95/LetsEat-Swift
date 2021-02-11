dadb4c649e2675a7a75e57226cfb8621a1f6542d
Doha Tubaileh <doha.tubaileh@gmail.com>
2021-02-11 12:53:43 +0200

    adding seque types

---
+++ Let's Eat/Let's Eat/Misc/Segue.swift
@@ -0,0 +1,20 @@
+//
+//  Segue.swift
+//  Let's Eat
+//
+//  Created by Doha Tubaileh on 10/2/2021.
+//
+
+import Foundation
+
+
+enum Segue:String {
+    case showDetail
+           case showRating
+           case showReview
+           case showAllReviews
+           case restaurantList
+           case locationList
+           case showPhotoReview
+           case showPhotoFilter
+}
